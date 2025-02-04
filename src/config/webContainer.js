29e04421a05b7283c658b9c00e16a1eb5e23770c
DINESH33A4 <dineshdcoc@gmail.com>
2025-02-05 02:07:02 +0530

    Added serve script to package.json

--- src/config/webContainer.js
+++ src/config/webContainer.js
@@ -3,9 +3,11 @@ import { WebContainer } from '@webcontainer/api';
 let webContainerInstance = null;
 
 
-export const getWebContainer = async () => {
+const getWebContainer = async () => {
     if (webContainerInstance === null) {
         webContainerInstance = await WebContainer.boot();
     }
     return webContainerInstance;
 }
+
+export default getWebContainer ;
